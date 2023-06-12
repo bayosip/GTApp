d296f29546a676937d956075a7098cd53fcba681
Adebayo <adebayojnr91@gmail.com>
2023-06-12 19:42:06 -0400

    - Optimize imports

--- app/src/main/java/com/speertech/testapp/presentation/view/screens/SearchScreen.kt
+++ app/src/main/java/com/speertech/testapp/presentation/view/screens/SearchScreen.kt
@@ -1,7 +1,6 @@
 package com.speertech.testapp.presentation.view.screens
 
 import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
